5de3e07b4dc177191d04301b98e2dfcec129dc26
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-11-11 16:58:42 +0100

    :rocket: Add src

---
+++ src/Entity/Service.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Aatis\DependencyInjection\Entity;
+
+class Service
+{
+    private ?object $instance = null;
+    /**
+     * @var array<string, mixed>
+     */
+    private array $givenArgs = [];
+    /**
+     * @var mixed[]
+     */
+    private array $args = [];
+    private static ?Container $container = null;
+
+    /**
+     * @param class-string $class
+     */
+    public function __construct(
+        private string $class,
+    ) {
+        $this->class = $class;
+    }
+
+    public static function setContainer(Container $container): void
+    {
+        self::$container = $container;
+    }
+
+    /**
+     * @param array<string, mixed> $givenArgs
+     */
+    public function setGivenArgs(array $givenArgs): void
+    {
+        $this->givenArgs = $givenArgs;
+    }
+
+    /**
+     * @param mixed[] $args
+     */
+    public function setArgs(array $args): void
+    {
+        $this->args = $args;
+    }
+
+    public function setInstance(object $instance): void
+    {
+        $this->instance = $instance;
+    }
+
+    private function instanciate(): void
+    {
+        if (!self::$container) {
+            throw new \Exception('Container not set');
+        }
+
+        $args = [];
+
+        foreach ($this->getDependencies() as $varName => $dependencyType) {
+            if (str_contains($dependencyType, '\\')) {
+                if (!self::$container->has($dependencyType)) {
+                    if (class_exists($dependencyType)) {
+                        $service = new Service($dependencyType);
+                        self::$container->set($dependencyType, $service);
+                        $service->instanciate();
+                    } else {
+                        throw new \Exception("Class $dependencyType not found");
+                    }
+                }
+                $args[] = self::$container->get($dependencyType);
+            } else {
+                $args[] = $this->givenArgs[$varName];
+            }
+        }
+
+        if (!empty($args)) {
+            $this->setArgs($args);
+        }
+
+        $this->instance = new ($this->class)(...$this->args);
+    }
+
+    public function getInstance(): object
+    {
+        if (self::$container && $this->class === self::$container::class) {
+            return self::$container;
+        }
+
+        if (!$this->instance) {
+            $this->instanciate();
+        }
+
+        /**
+         * @var object $instance
+         */
+        $instance = $this->instance;
+
+        return $instance;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDependencies(): array
+    {
+        $dependencies = [];
+        $reflexion = new \ReflectionClass($this->class);
+        $constructor = $reflexion->getConstructor();
+
+        if (!$constructor) {
+            return $dependencies;
+        }
+
+        $parameters = $constructor->getParameters();
+
+        foreach ($parameters as $parameter) {
+            $type = $parameter->getType();
+
+            if (!$type || !($type instanceof \ReflectionNamedType)) {
+                continue;
+            }
+
+            if (str_contains($type->getName(), '\\')) {
+                $dependencies[$parameter->getName()] = $type->getName();
+            } else {
+                $dependencies[$parameter->getName()] = $type->getName();
+            }
+        }
+
+        return $dependencies;
+    }
+
+    public function isInstancied(): bool
+    {
+        return $this->instance ? true : false;
+    }
+
+    /**
+     * @return array{
+     *  class: class-string,
+     *  args: mixed[]
+     * }
+     */
+    public function toArray(): array
+    {
+        return [
+            'class' => $this->class,
+            'args' => $this->args,
+        ];
+    }
+}
---
+++ src/Service/ContainerBuilder.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Aatis\DependencyInjection\Service;
+
+use Aatis\DependencyInjection\Entity\Service;
+use Aatis\DependencyInjection\Entity\Container;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @phpstan-type ServiceParams array<string, array{
+ *  arguments?: array<mixed>,
+ *  environment?: array<string>
+ * }>
+ * @phpstan-type YamlConfig array{
+ *  excludes?: array<int, string>,
+ *  services?: ServiceParams,
+ * }
+ * @phpstan-type ComposerJsonConfig array{
+ *  autoload: array{
+ *     psr-4: array<string, string>
+ *  },
+ * }
+ */
+class ContainerBuilder
+{
+    /**
+     * @var array<int, string>
+     */
+    private array $excludePaths = [];
+    /** @var ServiceParams */
+    private array $givenParams = [];
+    private Container $container;
+
+    /**
+     * @param array{
+     *  env: string
+     * } $ctx
+     */
+    public function __construct(
+        private readonly array $ctx,
+        private readonly string $sourcePath
+    ) {
+        $this->getConfig();
+    }
+
+    public function build(): Container
+    {
+        $this->container = new Container();
+        $this->registerFolder($this->sourcePath);
+
+        return $this->container;
+    }
+
+    private function registerFolder(string $folderPath): void
+    {
+        if (in_array($this->getShortPath($folderPath), $this->excludePaths)) {
+            return;
+        }
+
+        $folderContent = array_diff(scandir($folderPath) ?: [], ['..', '.']);
+
+        foreach ($folderContent as $element) {
+            $path = $folderPath.'/'.$element;
+
+            if (is_dir($path)) {
+                $this->registerFolder($path);
+
+                continue;
+            }
+
+            $this->register($path);
+        }
+    }
+
+    private function register(string $filePath): void
+    {
+        $shortPath = $this->getShortPath($filePath);
+
+        if (
+            !str_ends_with($shortPath, '.php')
+            || in_array($shortPath, $this->excludePaths)
+            || str_ends_with($shortPath, 'Interface.php')
+        ) {
+            return;
+        }
+
+        $namespace = $this->transformToNamespace($filePath);
+
+        if (
+            !class_exists($namespace)
+            || (new \ReflectionClass($namespace))->isAbstract()
+        ) {
+            return;
+        }
+        $service = new Service($namespace);
+
+        if (isset($this->givenParams[$namespace])) {
+            if (
+                isset($this->givenParams[$namespace]['environment'])
+                && !in_array($this->ctx['env'], $this->givenParams[$namespace]['environment'])
+            ) {
+                return;
+            }
+
+            if (isset($this->givenParams[$namespace]['arguments'])) {
+                $service->setGivenArgs($this->givenParams[$namespace]['arguments']);
+            }
+        }
+        $this->container->set($namespace, $service);
+    }
+
+    private function getShortPath(string $path): string
+    {
+        return str_replace($_ENV['DOCUMENT_ROOT'].'/../src', '', $path);
+    }
+
+    private function transformToNamespace(string $filePath): string
+    {
+        /** @var ComposerJsonConfig */
+        $composerJson = json_decode(file_get_contents($_ENV['DOCUMENT_ROOT'].'/../composer.json') ?: '', true);
+        $autoloaderInfos = $composerJson['autoload']['psr-4'];
+        $baseNamespace = array_key_first(array_filter($autoloaderInfos, fn ($value) => 'src/' === $value));
+        $temp = str_replace($_ENV['DOCUMENT_ROOT'].'/../src/', $baseNamespace ?? 'App\\', $filePath);
+        $temp = str_replace(DIRECTORY_SEPARATOR, '\\', $temp);
+        $temp = str_replace('.php', '', $temp);
+
+        return $temp;
+    }
+
+    private function getConfig(): void
+    {
+        if (file_exists($_ENV['DOCUMENT_ROOT'].'/../config/services.yaml')) {
+            /** @var YamlConfig */
+            $config = Yaml::parseFile($_ENV['DOCUMENT_ROOT'].'/../config/services.yaml');
+            $this->excludePaths = $config['excludes'] ?? [];
+            $this->givenParams = $config['services'] ?? [];
+        }
+    }
+}
