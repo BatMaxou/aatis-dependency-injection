4b2f2b0242ec452ff5caff71696eba0749c98861
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-12-09 16:03:45 +0100

    :arrow_up: Move to official psr container

---
+++ src/Exception/ContainerException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aatis\DependencyInjection\Exception;
+
+use Psr\Container\ContainerExceptionInterface;
+
+class ContainerException extends \Exception implements ContainerExceptionInterface
+{
+}
