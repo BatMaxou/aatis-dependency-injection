2fb03c1c7f8552b4d44a7095fe0e7afba6332ea5
BatMaxou <maximebatista.lycee@gmail.com>
2025-05-15 23:38:48 +0200

    :recycle: Move buildFromInterface to TagBuilder

--- src/Service/ServiceTagBuilder.php
+++ src/Service/ServiceTagBuilder.php
@@ -11,43 +11,6 @@ use Aatis\Tag\Service\TagBuilder;
  */
 class ServiceTagBuilder extends TagBuilder
 {
-    /**
-     * @param class-string[] $interfaces
-     * @param \BackedEnum[] $options
-     *
-     * @return ServiceTag[]|string[]
-     */
-    public function buildFromInterfaces(array $interfaces, $options = []): array
-    {
-        $tags = [];
-
-        foreach ($interfaces as $interface) {
-            $tags[] = $this->buildFromInterface($interface, $options);
-        }
-
-        return array_unique($tags);
-    }
-
-    /**
-     * @param class-string $interface
-     * @param \BackedEnum[] $options
-     */
-    public function buildFromInterface(string $interface, $options = []): ServiceTag|string
-    {
-        $explode = explode('\\', $interface);
-        $interface = $explode[count($explode) - 1];
-        if (str_ends_with($interface, 'Interface')) {
-            $interface = substr($interface, 0, -9);
-        }
-
-        $tag = implode(array_map(
-            fn ($letter) => ctype_upper($letter) ? sprintf('-%s', strtolower($letter)) : $letter,
-            str_split(lcfirst($interface)),
-        ));
-
-        return $this->buildFromName($tag, $options);
-    }
-
     protected function newTag(array $options): ServiceTag
     {
         $tag = new ServiceTag();
